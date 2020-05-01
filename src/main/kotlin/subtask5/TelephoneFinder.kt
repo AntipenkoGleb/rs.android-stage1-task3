8c06e29d767767b44f957a993b1e53970f2f1696
Antipenko Gleb <antipenko.g.d@gmail.com>
2020-05-02 01:07:10 +0300

    add solution for task5

--- src/main/kotlin/subtask5/TelephoneFinder.kt
+++ src/main/kotlin/subtask5/TelephoneFinder.kt
@@ -1,11 +1,34 @@
 package subtask5
 
-import kotlin.reflect.KClass
+import java.util.*
 
 class TelephoneFinder {
 
+    private val numbersNeighbors = hashMapOf(
+        '0' to arrayListOf('8'),
+        '1' to arrayListOf('2', '4'),
+        '2' to arrayListOf('1', '3', '5'),
+        '3' to arrayListOf('2', '6'),
+        '4' to arrayListOf('1', '5', '7'),
+        '5' to arrayListOf('2', '4', '6', '8'),
+        '6' to arrayListOf('3', '5', '9'),
+        '7' to arrayListOf('4', '8'),
+        '8' to arrayListOf('0', '5', '7', '9'),
+        '9' to arrayListOf('6', '8')
+    )
+
     // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+        if(number.contains('-')) return null
+        
+        val resultList = ArrayList<String>()
+        for ((index, char) in number.withIndex()) {
+            val newNumber = number.toCharArray()
+            for (neighbor in numbersNeighbors[char]!!) {
+                newNumber[index] = neighbor
+                resultList.add(newNumber.joinToString(""))
+            }
+        }
+        return resultList.toTypedArray()
     }
 }
