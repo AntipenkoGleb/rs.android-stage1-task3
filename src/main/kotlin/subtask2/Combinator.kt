8ee628af5d6e86f7d619f70eb41378921be02483
Antipenko Gleb <antipenko.g.d@gmail.com>
2020-05-05 22:35:25 +0300

    add solution for task2

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -4,6 +4,23 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val (c, n) = array
+        var k = 0
+        while (getCombinations(n, ++k) < c) {
+            if (k + 1 > n) return null
+        }
+        return k
+    }
+
+    private fun getCombinations(n: Int, k: Int): Long {
+        return getFactorial(n) / (getFactorial(n - k) * getFactorial(k))
+    }
+
+    private fun getFactorial(n: Int): Long {
+        var factorial = 1L
+        for (i in 1..n) {
+            factorial *= i
+        }
+        return factorial
     }
 }
