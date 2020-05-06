9bc2137789fcc7f4d9d550a088293f153fc088a6
Antipenko Gleb <antipenko.g.d@gmail.com>
2020-05-06 16:08:49 +0300

    add solution for task4

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -4,6 +4,28 @@ class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        val res = decompose(number.toLong() * number.toLong(), number, mutableListOf()).filterIsInstance<Int>().toTypedArray()
+        return if (res.isEmpty()) null else res
+    }
+
+    private fun decompose(ost: Long, step: Int, list: MutableList<Any>): List<Any> {
+        for (i in step - 1 downTo 1) {
+            val max: Long = i.toLong() * i.toLong()
+            if (max < ost) {
+                val arr = list.toMutableList().apply { add(i) }
+                val res = decompose(ost - max, i, arr)
+                if (res.contains("LAST")) {
+                    list.addAll(res)
+                    return res
+                }
+            }
+            if (ost == max) {
+                return list.apply {
+                    add(i)
+                    add("LAST")
+                }.reversed()
+            }
+        }
+        return emptyList()
     }
 }
